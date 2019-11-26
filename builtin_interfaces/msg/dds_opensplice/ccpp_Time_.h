b265bcd5b6dd3c914401c9683d6dfd6150cc713f
HiroiImanishi <imanishi@lab3.kuis.kyoto-u.ac.jp>
2019-11-26 10:37:29 +0900

    add makefile

---
+++ builtin_interfaces/msg/dds_opensplice/ccpp_Time_.h
@@ -0,0 +1,12 @@
+#ifndef CCPP_H_3D1ABD8B97CA639754430FD9BE1181EA_Time__H
+#define CCPP_H_3D1ABD8B97CA639754430FD9BE1181EA_Time__H
+
+#include "Time_.h"
+#undef OS_API
+#include "Time_Dcps.h"
+#undef OS_API
+
+#include <orb_abstraction.h>
+#include "Time_Dcps_impl.h"
+
+#endif /* CCPP_H_3D1ABD8B97CA639754430FD9BE1181EA_Time__H */
